507ebaac2eda56f922bc73a57b272815e2c104bd
lerrra <sniff451@gmail.com>
2020-06-10 20:25:18 +0300

    update button styles

--- frontend/src/components/CreateButton.js
+++ frontend/src/components/CreateButton.js
@@ -1,32 +1,16 @@
 import React from 'react';
 
+import '../styles/_border-button.css';
+
 class CreateButton extends React.Component {
     render() {
         return (
-            <button
-                style={{
-                    width: "100%",
-                    margin: "0.5rem 0",
-                    fontSize: "0.875rem",
-                    lineHeight:"1.5",
-                    fontWeight: "bold",
-
-                    padding: "0.5rem 2rem",
-
-                    background: "transparent",
-                    borderRadius: "8px",
-
-                    border: "1px solid #585EE6",
-                    outline: "none",
-                    cursor: "pointer",
-
-                    color: "#585EE6",
-                    textAlign: "center",
-                    textDecoration: "none"
-                }}
-                type="button">
-                Create room
-            </button>
+            <button className="border-button" type="button"
+                    style={{
+                        width: "100%",
+                        margin: "0.5rem 0"
+                    }}>
+                Create room</button>
         );
     }
 }
