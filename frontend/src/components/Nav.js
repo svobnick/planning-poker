ba0232b91b7afe97203af72b82a7e3c2688b2455
lerrra <sniff451@gmail.com>
2020-06-15 18:46:25 +0300

    simplify onClick function in 'Nav' component

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -8,28 +8,16 @@ class Nav extends React.Component {
     render() {
         return (
             <div className="nav">
-                <button className="nav__item" type="button" onClick={this.onLinkClickAbout().bind(this)}>About</button>
-                <button className="nav__item" type="button" onClick={this.onLinkClickRules().bind(this)}>How It Works</button>
-                <button className="nav__item" type="button" onClick={this.onLinkClickContacts().bind(this)}>Contacts</button>
+                <button className="nav__item" type="button" onClick={this.onLinkClick('/about').bind(this)}>About</button>
+                <button className="nav__item" type="button" onClick={this.onLinkClick("/rules").bind(this)}>How It Works</button>
+                <button className="nav__item" type="button" onClick={this.onLinkClick("/contacts").bind(this)}>Contacts</button>
             </div>
         );
     }
 
-    onLinkClickAbout() {
+    onLinkClick(path) {
         return function (e) {
-            this.props.history.push("/about")
-        }
-    }
-
-    onLinkClickRules() {
-        return function (e) {
-            this.props.history.push("/rules")
-        }
-    }
-
-    onLinkClickContacts() {
-        return function (e) {
-            this.props.history.push("/contacts")
+            this.props.history.push(path)
         }
     }
 }
