8d28070da076f8f457acd2feab8f03e9386efe03
lerrra <sniff451@gmail.com>
2020-06-15 22:19:48 +0300

    create main page and add 'Menu' component

---
+++ frontend/src/components/Menu.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import '../styles/layout/_menu.scss';
+import {withRouter} from "react-router";
+
+
+class Menu extends React.Component {
+    render() {
+        return (
+            <div className="menu">
+                <h1 className="menu__logo" onClick={this.onLinkClick('/').bind(this)}>Planning<br/> Poker</h1>
+            </div>
+        );
+    }
+
+    onLinkClick(path) {
+        return function (e) {
+            this.props.history.push(path)
+        }
+    }
+}
+
+export default withRouter(Menu);
--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import Menu from './Menu';
+
+import '../styles/layout/_poker.scss';
 
 class Room extends React.Component {
     render() {
         const {params} = this.props.match;
         const {roomId} = params
         return (
-            <div className="poker">Hello
+            <div className="poker">
+                <Menu />
                 <div className="roomId">RoomId: ({roomId})</div>
             </div>
         );
