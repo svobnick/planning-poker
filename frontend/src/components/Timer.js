853127ffa9cd48698ead284ab7f4bbc6ce481f40
lerrra <sniff451@gmail.com>
2020-06-16 15:56:39 +0300

    set timer function

--- frontend/src/components/Timer.js
+++ frontend/src/components/Timer.js
@@ -3,11 +3,44 @@ import React from 'react';
 import '../styles/layout/_timer.scss';
 
 class Timer extends React.Component {
+    state = {
+        timerOn: false,
+        timerStart: 0,
+        timerTime: 0
+    };
+
+    startTimer() {
+        this.setState({
+            timerOn: true,
+            timerTime: this.state.timerTime,
+            timerStart: Date.now() - this.state.timerTime
+        });
+        this.timer = setInterval(() => {
+            this.setState({
+                timerTime: Date.now() - this.state.timerStart
+            });
+        }, 10);
+    };
+
+    stopTimer = () => {
+        this.setState({ timerOn: false });
+        clearInterval(this.timer);
+    };
+
     render() {
+        const {timerTime} = this.state;
+        let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
+        let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
+
         return (
             <div className="timer">
-                <div className="timer__counter">01:29</div>
-                <button className="timer__button">Stop</button>
+                <div className="timer__counter">{minutes}:{seconds}</div>
+                {this.state.timerOn === false && this.state.timerTime === 0 && (
+                    <button className="timer__button" onClick={this.startTimer.bind(this)}>Start</button>
+                )}
+                {this.state.timerOn === true && (
+                    <button className="timer__button" onClick={this.stopTimer.bind(this)}>Stop</button>
+                )}
             </div>
         );
     }
