a7d4e3eaadad4d87d45bee3785616972f3753fa0
lerrra <sniff451@gmail.com>
2020-07-13 19:18:55 +0300

    update design of 'contacts' page

--- frontend/src/components/Contacts.js
+++ frontend/src/components/Contacts.js
@@ -4,29 +4,34 @@ import '../styles/layout/_container.scss';
 import '../styles/layout/_head.scss';
 import '../styles/layout/_contacts.scss';
 
-
-import Logo from '../images/ill.png';
+import Menu from "./Menu";
+import Back from "./Back";
 
 
 class Contacts extends React.Component {
     render() {
         return (
-            <div className="start">
-                <div className="container" style={{backgroundColor:"#ECEEFF"}}>
-                    <div className="head">
-                        <h1 className="head__title">Planning Poker</h1>
-                        <img className="head__image" src={Logo} alt="poker logo"/>
-                    </div>
-                    <div className="contacts">
-                        <h2 className="contacts__title">Get in touch</h2>
-                        <p className="contacts__text">
-                            We'd love to hear how to make Planning Poker better for you.
-                            Feel free to write us:
-                        </p>
-                        <a className="contacts__link" href="mailto:svobnick@ya.ru">planningpoker@ask.io</a>
+            <div className="about">
+                <Menu/>
+                <div className="about__head">
+                    <div className="about__head-text">
+                        <h1 className="about__head-text-title">Get in touch</h1>
+                        <p className="about__head-text-lead">We'd love to hear how to make Planning Poker better for you — feel free to write us</p>
                     </div>
                 </div>
 
+                <div className="about__text">
+                    <div className="contacts">
+                        <h4 className="contacts__title"><span role="img" aria-label="letter">💌</span> Email</h4>
+                        <a className="contacts__text" href="mailto:svobnick@ya.ru">planningpoker@ask.io</a>
+                    </div>
+                    <div className="contacts">
+                        <h4 className="contacts__title"><span role="img" aria-label="robot">🤖</span> Project on Github</h4>
+                        <a className="contacts__text" href="https://github.com/svobnick/planning-poker" target="_blank">svobnick/planning-poker</a>
+                    </div>
+                </div>
+
+                <Back/>
             </div>
         );
     }
